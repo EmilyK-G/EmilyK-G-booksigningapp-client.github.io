d88bb87b5add912916f0258671358998e375bdfa
Emily.Garcia <emilikimberly1@gmail.com>
2022-08-22 11:27:25 -0400

    Pagination bug

--- src/components/NavBtn/NavBtn.jsx
+++ src/components/NavBtn/NavBtn.jsx
@@ -1,54 +1,46 @@
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from "react-router-dom";
 import { Outlet, Link } from "react-router-dom";
 import Button from 'react-bootstrap/Button';
-import { GrPrevious, GrNext } from 'react-icons/gr';
 import './NavBtn.css';
 
 function NavBtn() {
-  const [pageCount, setPageCount] = useState(1);
+  const [nextPage, setNextPage] = useState('/');
+  const [prevPage, setPrevPage] = useState('/');
+  const [clicked, setClicked] = useState(0);
 
   const location = useLocation();
+  const pathname = location.pathname;
+  // const pageRef = useRef(pathname);
 
-  const pageRef = useRef(location.pathname);
+  // useEffect(() => {
+  //   if(localStorage.getItem('pageName') !== null){
+  //     pageRef.current = JSON.parse(localStorage.getItem('pageName'));
+  //   }
+  // }, [])
 
-  useEffect(() => {
-    if(localStorage.getItem('pageName') !== null){
-      pageRef.current = JSON.parse(localStorage.getItem('pageName'));
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('pageName', JSON.stringify(location.pathname))
-  }, [location.pathname])
-    
-  useEffect(()=>{
-    if(pageRef.current === '/'){
-      setPageCount(1)
-    }
-    if(pageRef.current === '/users'){
-      setPageCount(2)
-    }
-    if(pageRef.current === '/books'){
-      setPageCount(3)
-    }
-    console.log(pageRef)
-  }, [pageRef])
-
-  function handlePrevClick() {
-    setPageCount(pageCount - 1);
-  }
-
-  function handleNextClick() {
-    setPageCount(pageCount + 1);
-    console.log(pageCount)
-  }
-
+  // useEffect(() => {
+  //   localStorage.setItem('pageName', JSON.stringify(location.pathname))
+  // }, [location.pathname])
   
+  useEffect(() => {
+    if(pathname === '/'){
+      setNextPage('/users')
+    }
+    if(pathname === '/users'){
+      setNextPage('/books')
+      setPrevPage('/')
+    }
+    if(pathname === '/books'){
+      setPrevPage('/users')
+    }
+    console.log(pathname)
+  }, [pathname, clicked])
+
   return (
     <>  
-        <Button className={'prev_btn_style' + (pageCount === 1 ? ' btn_dissapear' : '') } onClick={()=>{handlePrevClick()}}><Link to={pageCount === 2 ? '/' : pageCount === 3 ? '/users' : false}><GrPrevious /></Link></Button>
-        <Button className={'next_btn_style' + (pageCount === 3 ? ' btn_dissapear' : '') } onClick={()=>{handleNextClick()}}><Link to={pageCount === 1 ? '/users' : pageCount === 2 ? '/books' : false}><GrNext /></Link></Button>
+        <Button className='prev_btn_style d-flex align-items-end' onClick={()=>setClicked(clicked - 1)}><Link className={(pathname === '/' ? ' btn_dissapear' : '')} to={prevPage}>{`<`}</Link></Button>
+        <Button className='next_btn_style d-flex align-items-end' onClick={()=>setClicked(clicked + 1)}><Link to={nextPage} className={(pathname === '/books' ? ' btn_dissapear' : '')}>{`>`}</Link></Button>
         <Outlet />
     </>
   )
--- src/components/Users/LoginModal.jsx
+++ src/components/Users/LoginModal.jsx
@@ -10,13 +10,13 @@ function LoginModal(props) {
           aria-labelledby="contained-modal-title-vcenter"
           centered
         >
-          <Modal.Header closeButton>
+          <Modal.Header>
             <Modal.Title id="contained-modal-title-vcenter">
-              Modal heading
+              Hi Perry!
             </Modal.Title>
           </Modal.Header>
           <Modal.Body>
-            <h4>Centered Modal</h4>
+            <h4>Please enter your PIN</h4>
             <p>
               Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
               dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
