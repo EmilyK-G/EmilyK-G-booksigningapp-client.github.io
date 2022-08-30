4efc447cf00cc46b6034dc83cc5c9fb9a2866bc2
Emily.Garcia <emilikimberly1@gmail.com>
2022-08-30 13:12:38 -0400

    Dashboard second commit

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,11 +1,27 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../contexts/UserContext';
+import { SignatureContext } from '../../contexts/SignatureContext';
+import { IoIosLogOut } from 'react-icons/io';
+import { GrEdit } from 'react-icons/gr';
 import './Dashboard.css';
+import { useEffect } from 'react';
 
 function Dashboard() {
-  const{loggedUser, setLoggedIn} = useContext(UserContext);
+  const{loggedUser, setLoggedIn, usersArr} = useContext(UserContext);
+  const{booksSigned, setBooksSigned} = useContext(SignatureContext);
+  const [booksToSign, setBooksToSign] = useState(4);
   const navigate = useNavigate();
+
+  useEffect(()=>{
+    const usrs = usersArr.length -1;
+    if(booksSigned >= 1){
+      const toSign = usrs - booksSigned;
+      setBooksToSign(toSign)
+    } else{
+        setBooksToSign(usrs)
+    }
+  }, [usersArr.length, booksSigned])
 
   function userLogout() {
     setLoggedIn(false);
@@ -14,24 +30,27 @@ function Dashboard() {
 
   return (
     
-    <div className='d-flex flex-column' >
+    <div className='d-flex flex-column dshbrd_container' >
         <button 
-          variant='outline'
-          className='btn btn-outline-danger align-self-end logout_btn' 
+          className='btn align-self-end d-flex align-items-center logout_btn' 
           onClick={()=>{userLogout()}}>
-          Log Out
+          <IoIosLogOut />
         </button>
-        <div className='d-flex flex-column profile_div_1 py-4'>
-          <div className='profile_pic_div m-4 align-self-center' style={{backgroundImage: `url(${loggedUser.Picture})`}}></div>
+        <div className='d-flex flex-column justify-content-center align-items-center profile_div_1 py-4'>
+          <div className='profile_pic_div m-4' style={{backgroundImage: `url(${loggedUser.Picture})`}}></div>
           <h3 className='profile_name'>Hi {loggedUser.Name}!</h3>
         </div>
-        <div className="d-flex flex-column profile_div_2 py-4">
-          <p className='align-self-start mt-3 mb-0 div_2_text_1'>You still have</p>
-          <div className='books_left_num align-self-end d-flex justify-content-center align-items-center'>4</div>
-          <p className='align-self-end div_2_text_2'>books to sign</p>
+        <div className="d-flex flex-column justify-content-center profile_div_2 py-4">
+          <p className='align-self-start div_2_text_1'>You still have</p>
+          <div className='books_left_num align-self-end d-flex justify-content-center align-items-center'>{booksToSign}</div>
+          <p className='align-self-end div_2_text_2 '>book{booksToSign >= 1 ? 's':''} to sign</p>
         </div>
-        <div className="d-flex flex-column profile_div_3 py-4">
-          <p className="div_3_text">Signing as: {loggedUser.Signature}</p>
+        <div className="d-flex flex-column justify-content-center profile_div_3 pb-2">
+          <p className="div_3_text_1 d-flex align-content-start mx-4">Signing as: </p>
+          <div className="div_3_signature_container mx-4 mb-0 d-flex align-items-end justify-content-end">
+            <div className="div_3_text_signature d-flex justify-content-end">{loggedUser.Signature}</div>
+            <div className='btn_edit_signature'><GrEdit/></div>
+          </div>
         </div>
       </div>
   )
--- src/contexts/SignatureContext.jsx
+++ src/contexts/SignatureContext.jsx
@@ -4,9 +4,10 @@ const SignatureContext = createContext();
 
 const SignatureContextProvider = ({children})=> {
     const [signing, setSigning] = useState({});
+    const [booksSigned, setBooksSigned] = useState(0);
 
     return (
-        <SignatureContext.Provider value={{signing, setSigning}}>
+        <SignatureContext.Provider value={{signing, setSigning, booksSigned, setBooksSigned}}>
             {children}
         </SignatureContext.Provider>
         )
