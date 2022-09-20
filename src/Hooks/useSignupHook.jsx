2914efc3940557e9dbd92bf53c102a1290ba66e5
Emily.Garcia <emilikimberly1@gmail.com>
2022-09-20 11:34:15 -0400

    Signup form functionality

---
+++ src/Hooks/useSignupHook.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { useUserContext } from "./UserContextHook";
+
+export const useSignup = ()=> {
+    const[error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { dispatch } = useUserContext()
+
+    const signup = async(name, last_name, email, pin, class_of, img, signature) => {
+        setIsLoading(true)
+        setError(null)
+
+        const response = await fetch('/api/user/signup', {
+            method:'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({name, last_name, email, pin, class_of, img, signature})
+        })
+        const json = await response.json()
+
+        if (!response.ok){
+            setIsLoading(false)
+            setError(json.error)
+        }
+        if (response.ok){
+            //save the user to local storage
+            localStorage.setItem('user', JSON.stringify(json))
+
+            //update UserContext
+            dispatch({type:'LOGIN', payload: json})
+
+            setIsLoading(false)
+        }
+    }
+
+    return {signup, isLoading, error}
+}
--- src/components/Signup/Signup.jsx
+++ src/components/Signup/Signup.jsx
@@ -1,35 +1,57 @@
 import { useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import { useSignup } from '../../Hooks/useSignupHook';
 
 function Signup() {
     const [email, setEmail] = useState('');
     const [pin, setPin] = useState('');
+    const [name, setName] = useState('');
+    const [last_name, setLast_name] = useState('');
+    const [class_of, setClass_of] = useState('');
+    const [img, setImg] = useState('');
+
+    const {signup, error, isLoading} = useSignup()
 
     const handleSubmit = async (e)=>{
         e.preventDefault()
-
-        console.log(email, pin)
+        const signature = name + ' ' + last_name;
+        await signup(name, last_name, email, pin, class_of, img, signature)
     }
 
     return (
         <form onSubmit={handleSubmit}>
             <div className="form-row">
-            <div className="col">
-                <input type="text" className="form-control" placeholder="First name"/>
-            </div>
-            <div className="col">
-                <input type="text" className="form-control" placeholder="Last name"/>
-            </div>
+                <div className="form-group">
+                    <select className="form-control" onChange={(e)=>setClass_of(e.target.value)} value={class_of}>
+                        <option>Select your class</option>
+                        <option>June 2022</option>
+                        <option>July 2022</option>
+                        <option>August 2022</option>
+                    </select>
+                </div>
+                <div className="col">
+                    <input type="text" onChange={(e)=>setName(e.target.value)} value={name} className="form-control" placeholder="First name"/>
+                </div>
+                <div className="col">
+                    <input type="text" onChange={(e)=>setLast_name(e.target.value)} value={last_name} className="form-control" placeholder="Last name"/>
+                </div>
             </div>
             <div className="form-group">
-                <label htmlFor="exampleInputEmail1">Email address</label>
+                <label>Email address</label>
                 <input type="email" onChange={(e)=>setEmail(e.target.value)} className="form-control" value={email} placeholder="Enter email"/>
                 <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
             </div>
             <div className="form-group">
-                <label htmlFor="exampleInputPassword1">PIN</label>
-                <input type="password" onChange={(e)=>setPin(e.target.value)} value={pin} className="form-control" placeholder="Password"/>
+                <label>PIN</label>
+                <input type="number" onChange={(e)=>setPin(e.target.value)} value={pin} className="form-control" placeholder="Password"/>
             </div>
-            <button type="submit" className="btn btn-primary">Submit</button>
+            <div className="form-group">
+                <label>Upload your picture here...</label>
+                <input type="file" onChange={(e)=>setImg(e.target.value)} value={img} className="form-control-file"/>
+                <small className="form-text text-muted">This will be your face.</small>
+            </div>
+            <button disabled={isLoading} type="submit" className="btn btn-primary">Submit</button>
+            {error && <Alert variant="danger">{error}</Alert>}
         </form>
     )
 }
