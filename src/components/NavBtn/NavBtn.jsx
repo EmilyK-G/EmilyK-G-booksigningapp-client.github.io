b45075dc6b4cde0cfa3dff91f7e24f6f240265be
Emily.Garcia <emilikimberly1@gmail.com>
2022-09-23 20:12:35 -0400

    Styling

--- src/components/NavBtn/NavBtn.jsx
+++ src/components/NavBtn/NavBtn.jsx
@@ -5,6 +5,7 @@ import Welcome from '../Welcome/Welcome';
 import LoginModal from './LoginModal';
 import MyButton from './MyButton';
 import { Outlet } from "react-router-dom";
+import { motion } from 'framer-motion';
 import './NavBtn.css';
 
 function NavBtn() {
@@ -14,15 +15,15 @@ function NavBtn() {
   const hideNext = pathname === '/users';
   
   return (
-    <div className='d-flex flex-column align-items-center'>  
-        {pathname === '/' && <Welcome />}
-        <LoginModal userSelected={userSelected}/>
-        <div className='d-flex justify-content-between my_btn_div'>
-            <MyButton linkTo={prevPage} sign={hidePrev ? '' : '<'}/>
-            <MyButton linkTo={nextPage} sign={hideNext ? '' : '>'}/>
-        </div>
-        <Outlet />
-    </div>
+      <div className='d-flex flex-column align-items-center'>  
+          {pathname === '/' && <Welcome />}
+          <LoginModal userSelected={userSelected}/>
+          <div className='d-flex justify-content-between my_btn_div'>
+              <MyButton linkTo={prevPage} sign={hidePrev ? '' : '<'}/>
+              <MyButton linkTo={nextPage} sign={hideNext ? '' : '>'}/>
+          </div>
+          <Outlet />
+      </div>
   )
 }
 
--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -6,10 +6,17 @@ import './Welcome.css';
 function Welcome() {
 
   return (
-    <div className='welcome_content mt-4'>
-      <h2>Welcome class of 2022!</h2>
-      <Signup />
-    </div>
+    <motion.div 
+        initial= {{opacity: 0, x:-100}}
+        animate= {{opacity: 1, x: 0}}
+        exit= {{opacity:0, x:-100}}
+        transition={{ duration: 0.2 }}>
+          <div className='welcome_content mt-4'>
+            <h2>Welcome class of 2022!</h2>
+            <Signup />
+          </div>
+    </motion.div>
+    
   )
 }
 
