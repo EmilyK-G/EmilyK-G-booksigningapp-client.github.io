ff3445ee04e5aebc716cf654b8a1de1642ea37b4
Emily.Garcia <emilikimberly1@gmail.com>
2022-10-18 16:50:29 -0400

    ask before delete

--- src/components/Messages/PrevMessages/PrevMessages.jsx
+++ src/components/Messages/PrevMessages/PrevMessages.jsx
@@ -3,13 +3,19 @@ import { useUserContext } from "../../../Hooks/useUserContextHook";
 import { IconContext } from "react-icons";
 import { formatRelative } from 'date-fns';
 import { RiChatDeleteFill } from 'react-icons/ri';
+import Overlay from 'react-bootstrap/Overlay';
 import './PrevMessages.css';
+import { useState, useRef } from "react";
+import Tooltip from 'react-bootstrap/Tooltip';
 
 
 function PrevMessages({mes}) {
 
     const {dispatch} = useSignatureContext();
     const {user} = useUserContext();
+
+    const [show, setShow] = useState(false);
+    const target = useRef(null);  
 
     const handleDeleteMes = async() => {
         if (!user) {
@@ -27,6 +33,7 @@ function PrevMessages({mes}) {
 
         if (response.ok) {
             dispatch({type: 'DELETE_SIGNATURE', payload: json})
+            setShow(false)
         }
     }
 
@@ -39,8 +46,15 @@ function PrevMessages({mes}) {
                     <cite title="Sent date">{formatRelative (new Date(mes.updatedAt), new Date())}</cite>
                 </figcaption>
                 <IconContext.Provider value={{ color: '#ea1537', className: "delete_icon" }}>
-                    <span className="text-end delete_msg_btn" onClick={handleDeleteMes}><RiChatDeleteFill/></span>
+                    <span ref={target} className="text-end delete_msg_btn" onClick={()=>setShow(!show)}><RiChatDeleteFill/></span>
                 </IconContext.Provider>
+                <Overlay target={target.current} show={show} placement="left" className='my_tooltip'>
+                <Tooltip style={{transform:'translate3d(0px, 85px, 0px)'}}>
+                    <p>{`Do you want to delete this message? (You cannot undo this later)`}</p>
+                    <button type="button" className='btn-small btn btn-danger' onClick={()=>handleDeleteMes()}>YES, delete</button>
+                    <button type="button" className='btn-small btn btn-secondary' onClick={()=>setShow(false)}>NO, wait</button>
+                </Tooltip>
+                </Overlay>
             </figure>
     
 
