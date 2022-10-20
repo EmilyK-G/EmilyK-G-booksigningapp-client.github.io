04253399ab4993e976e21fb1b7a0e83916b9b5d5
Emily.Garcia <emilikimberly1@gmail.com>
2022-10-20 17:10:41 -0400

    first commit

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,7 @@ app.use('/api/user', userRoutes)
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     // listen for request
-    app.listen(process.env.PORT, () =>{
+    app.listen(process.env.PORT || 4000, () =>{
         console.log('connected to db & listening on port', process.env.PORT)
     })
 })
