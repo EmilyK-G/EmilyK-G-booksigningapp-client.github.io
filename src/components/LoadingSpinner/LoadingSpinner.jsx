cee576e8e18011bf82a6377d0a9eee96dfaff07f
Emily.Garcia <emilikimberly1@gmail.com>
2022-11-03 16:39:32 -0400

    loading style

--- src/components/LoadingSpinner/LoadingSpinner.jsx
+++ src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,23 +1,28 @@
-import React from 'react';
+import { motion } from 'framer-motion';
 import './LoadingSpinner.css';
 
 const LoadingSpinner = ({loadingPage}) => {
   return (
-    <div className="center">
-      <div className="waves_container">
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-        <div className="wave"></div>
-      </div>
-      <p>Loading {loadingPage}...</p>
-    </div>
+    <motion.div 
+      initial= {{opacity: 0, y:-100, x:0}}
+      animate= {{opacity: 1, y:0, width:'95%', x:0}}
+      exit= {{opacity:0, y:100,width:'85%', x:0}}
+      transition={{ duration: 0.5 }}
+      className='center'>
+        <div className="waves_container">
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+          <div className="wave"></div>
+        </div>
+        <p>Loading {loadingPage}...</p>
+    </motion.div>
   )
 }
 
