055070e4c0c97e64f43c9a5b75d69238e519aac6
Emily.Garcia <emilikimberly1@gmail.com>
2022-11-04 17:50:21 -0400

    user selected bug

--- src/Hooks/useLoginHook.jsx
+++ src/Hooks/useLoginHook.jsx
@@ -5,19 +5,19 @@ import { useUserContext } from "./useUserContextHook";
 export const useLogin = ()=>{
     const [error, setError] = useState(null)
     const [isLoading, setIsLoading] = useState(null)
-    const { dispatch } = useUserContext()
+    const { dispatch, setUserSelected } = useUserContext()
 
     const login = async (email, pin)=>{
-        setIsLoading(true)
-        setError(null)
+        setIsLoading(true);
+        setError(null);
 
         const response = await fetch('https://booksigning.onrender.com/api/user/login', {
             method: 'POST',
             mode: 'cors', 
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({email, pin})
-        })
-        const json = await response.json()
+        });
+        const json = await response.json();
 
         if(!response.ok){
             setIsLoading(false)
@@ -25,12 +25,12 @@ export const useLogin = ()=>{
         }
 
         if(response.ok){
-            localStorage.setItem('user', JSON.stringify(json))
+            localStorage.setItem('user', JSON.stringify(json));
 
-            dispatch({type: 'LOGIN', payload: json})
+            dispatch({type: 'LOGIN', payload: json});
 
-            setIsLoading(false)
-            console.log(json)
+            setIsLoading(false);
+            setUserSelected([])
         }
     }
 
